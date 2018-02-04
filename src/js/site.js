85dcc327260ddddce005c2a9b02fa01e92aa54c3
ash henry <ashleigh.h@mmtdigital.co.uk>
2018-02-04 14:17:34 +0000

    fix nav

--- src/js/site.js
+++ src/js/site.js
@@ -1,101 +1,79 @@
-console.log('ready');
+
+
+
 fetch('./js/tree.json').then(r => r.json())
 .then(data => {
 
     const tree = data;
     const siteMap = tree.children;
     const lone = document.getElementsByClassName('level__one')[0];
-    const ltwo = document.getElementsByClassName('level__two')[0];
 
-     const root = siteMap.filter(x=> {
-        return x;
-     });
-
-     const setRoot = root.filter( x=> {
-         if(x.linkPart === 'vehicles') { //inject this value;  root
-             return x.children;
-         }
-     });
-
-     const levelThree = setRoot.map(x=> {
-         return x.children;
-     });
-
-
-
-     const rootOption = setRoot[0];
-     const l1a = setRoot[0].children[0].children;
-     const l2 = setRoot[0].children;
-     const l3 = levelThree[0][0].children;
-
-     //const l4 = levelFour[0];
-    const levelFour = l3.filter(x=> {
-        return x.children;
-    });
-    const byoption =l2.filter(x=>{
-        if (x.linkPart ==='range-rover') {
-            return x.children;
+    function parseNodes(nodes) { // creates <ul>
+        let ul = document.createElement("UL");
+        for(let x in nodes) {
+            ul.appendChild(parseNode(nodes[x]));
         }
-    });
-    const bo = byoption[0].children;
+        return ul;
+    }
 
-    const createFullTree = ()=> {
-        const lev2 = document.getElementsByTagName('level2')[0];
-        [...l2].forEach((e)=> {
-            if(e.children !=null) {
-                e.children.filter(p=> {
-                    lone.innerHTML +=`<ul class="level2"><li>${p.title}</li></ul>`;
-                    if (p.children != null) {
-                        p.children.filter( g=> {
-                            lev2.innerHTML +=`<ul class="level3"><li>${g.title}</li></ul>`;
-                        });
+    function parseNode(node) { // creates <li>
+        let li = document.createElement("LI");
+        li.className = node.linkPart;
+        li.innerHTML = `<a href="${node.linkPart}">${node.title}</a>`;
+
+        if(node.children) li.appendChild(parseNodes(node.children));
+        return li;
+    }
+
+    lone.appendChild(parseNodes(siteMap));
+
+    const root = document.querySelectorAll('div.level__one > ul > li ');
+    const level1 = document.querySelectorAll('div.level__one > ul > li > ul > li');
+    const level2 = document.querySelectorAll('div.level__one > ul > li > ul > li > ul > li' );
+    const level3 = document.querySelectorAll('div.level__one > ul > li > ul > li > ul > li > ul > li' );
+    const uls = [];
+
+   function openlevel2(list, classname) {
+
+
+           [...list].forEach(item => {
+               item.parentElement.classList.add(classname);
+               const o = item.querySelectorAll('ul')[0];//get target ul
+               uls.push(o);
+
+
+               const showul = (e) => {
+                   if (item.querySelectorAll('ul')[0] !== undefined) {
+                       item.querySelectorAll('ul')[0].classList.add('active');
+
                     }
+               };
 
-                });
-            }
-           lone.innerHTML += `<li>${e.title}</li>`
-        });
-    };
+               const hideul = (e) => {
+                   const d =e.target.querySelectorAll('ul')[0];
 
-    createFullTree();
-    const createList = (e)=> {
-        //console.log('j');
-        [...l2].forEach(item=> {
-            //lone.innerHTML += `<li>${item.title}</li>`;
-        });
-    };
+                   if ( d !== undefined && d.classList.contains('active') === true) {
+                       d.classList.remove('active');
+                   }
 
-    const cl2 = ()=> {
-        [...bo].forEach(item=> {
-            //ltwo.innerHTML += `<li>${item.title}</li>`;
-        });
-    };
+               };
 
-    createList();
-    cl2();
+               item.addEventListener('mouseenter', showul);
+               item.addEventListener('mouseleave', hideul);
 
+           });
 
+   }
 
-
-     //const l5 = l4[0].children;
-
-     //console.log(levelFour);
-     //console.log(levelTwo);
-     //console.log(levelThree);
-     //console.log(l1a);
-     //console.log(rootOption);
-     console.log(l2);
-     //console.log(bo);
-     //console.log(l3);
-     //console.log(l4);
-     //console.log(l5);
-
-
-
+   openlevel2(root, 'root');
+   openlevel2(level1, 'level1');
+   openlevel2(level2 , 'level2');
+   openlevel2(level3, 'level3');
 
 
 })
 .catch(e => console.log(e));
+
 
 
 
