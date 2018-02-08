01a7b35b7be9a3041d416ebc7031e322c9174b0f
ash henry <ashleigh.h@mmtdigital.co.uk>
2018-02-08 08:14:00 +0000

    remove console.log

--- src/js/site.js
+++ src/js/site.js
@@ -4,6 +4,25 @@ fetch('./js/tree.json').then(r => r.json())
     const siteMap = data.children;
     const lone = document.getElementsByClassName('level__one')[0];
 
+    const tree = siteMap.filter( x => {
+       if (x.linkPart === 'vehicles') {
+           return x;
+       }
+    });
+
+    const parent = tree[0].children.filter( x => {
+        if (x.linkPart === 'range-rover') {
+            return x;
+        }
+    });
+
+    const child = parent[0].children.filter( x => {
+        if (x.children !== undefined) {
+            return x;
+        }
+    });
+
+    console.log(child);
     function parseNodes(nodes) { // creates <ul>
         let ul = document.createElement("UL");
         for(let x in nodes) {
@@ -20,12 +39,51 @@ fetch('./js/tree.json').then(r => r.json())
         return li;
     }
 
+
     lone.appendChild(parseNodes(siteMap));
+    const play = document.getElementsByClassName('target')[0];
+    const newel = document.createElement('div');
+    newel.className = 'newel';
+    newel.innerHTML=`<p>hi there</p>
+        <h2>h2222</h2>
+        <h3>h3333</h3>
+        <ul></ul>
+    `;
+    play.appendChild(newel);
+
+
+
+    const link = document.getElementsByClassName('link')[0];
+
+    const clickHandler = (e)=> {
+        e.preventDefault();
+        const pat = document.createElement('ul');
+        pat.innerHTML =`<li>blah blah blah</li>`;
+
+        const r = newel.childNodes;
+        [...r].filter(x => {
+            if(x.tagName === 'UL') {
+                x.remove();
+            }
+        });
+        newel.appendChild(pat);
+
+    };
+    link.addEventListener('click', clickHandler);
+
+
+
 
     const root = document.querySelectorAll('div.level__one > ul > li ');
     const level1 = document.querySelectorAll('div.level__one > ul > li > ul > li');
     const level2 = document.querySelectorAll('div.level__one > ul > li > ul > li > ul > li' );
     const level3 = document.querySelectorAll('div.level__one > ul > li > ul > li > ul > li > ul > li' );
+
+
+    const gh = (e) => {
+        window.location.href='www.google.com';
+    };
+    level2[0].addEventListener('click', gh);
 
    function openlevel(list, classname) {
 
@@ -47,7 +105,9 @@ fetch('./js/tree.json').then(r => r.json())
                    const menu = e.target.querySelectorAll('ul')[0];
 
                    if ( menu !== undefined && menu.classList.contains('active') === true) {
+
                        menu.classList.remove('active');
+                       menu.parentNode.classList.add('active');
                    }
                };
 
